ed1ab9e64926b0a1c2914e992db98211690b2504
Abdulrhman Alaa <abdoalaaabdo17@gmail.com>
2025-08-09 20:29:26 +0300

    fix Auto play switch dir

--- src/components/story/AutoplayToggle.tsx
+++ src/components/story/AutoplayToggle.tsx
@@ -1,5 +1,6 @@
 import { Switch } from '@/components/ui/switch'
 import { Label } from '@/components/ui/label'
+import { useTranslation } from 'react-i18next'
 
 interface AutoplayToggleProps {
   isAutoplay: boolean
@@ -12,6 +13,15 @@ export const AutoplayToggle = ({
   onAutoplayChange,
   currentSectionDir,
 }: AutoplayToggleProps) => {
+  const { i18n } = useTranslation()
+  const isRTL = i18n.language === 'ar'
+
+  const setSwitchDir = () => {
+    if (isRTL) {
+      if (currentSectionDir === 'ltr') return 'rtl'
+    } else return currentSectionDir
+  }
+
   return (
     <div
       className="flex items-center space-x-2 rtl:space-x-reverse"
@@ -21,7 +31,7 @@ export const AutoplayToggle = ({
         id="autoplay"
         checked={isAutoplay}
         onCheckedChange={onAutoplayChange}
-        dir={currentSectionDir}
+        dir={setSwitchDir()}
       />
       <Label htmlFor="autoplay" className="text-sm font-medium">
         Autoplay
